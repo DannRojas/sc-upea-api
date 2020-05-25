a33d1fde24fac1119d71fed718b75dca23e24a84
DannRojas <rojasc.dan@gmail.com>
2020-05-25 11:58:44 -0400

    Last commit

--- server/create-lb-tables.js
+++ server/create-lb-tables.js
@@ -1,6 +1,7 @@
 var server = require('./server');
 var ds = server.dataSources.mysql;
-var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'administrador', 'persona', 'capacitacion', 'estudiante', 'inscripcion'];
+// var lbTables = ['User', 'AccessToken', 'ACL', 'RoleMapping', 'Role', 'administrador', 'persona', 'capacitacion', 'estudiante', 'inscripcion'];
+var lbTables = ['estudiante'];
 ds.automigrate(lbTables, function(er) {
   if (er) throw er;
   console.log('Loopback tables [' + lbTables + '] created in ', ds.adapter.name);
